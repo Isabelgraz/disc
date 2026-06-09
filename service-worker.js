ef7032df3e410e297231b4c191f4902fd63c0d77
Isabel Grazina <isabelgraz@gmail.com>
2026-06-09 22:48:45 +0100

    Add files via upload
    Signed-off-by: Isabel Grazina <isabelgraz@gmail.com>

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE = 'disc-ig-v1';
+const ASSETS = [
+  '/disc/',
+  '/disc/index.html',
+  '/disc/manifest.json',
+  '/disc/icons/icon-192.png',
+  '/disc/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
